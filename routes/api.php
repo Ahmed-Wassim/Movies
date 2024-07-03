b9dfccabf0e0633b6a50b85f8f53c95bf6b0b9eb
Ahmed-Wassim <ahmedwassim317@gmail.com>
2024-07-04 00:28:58 +0300

    Implement Auth functionality

--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,24 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SettingController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\AdminController;
-use App\Http\Controllers\SettingController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api')->name('logout');
+    Route::post('/refresh', [AuthController::class, 'refresh'])->middleware('auth:api')->name('refresh');
+    Route::post('/me', [AuthController::class, 'me'])->middleware('auth:api')->name('me');
+});
 
 
-Route::prefix('dashboard')->group(function () {
+Route::middleware('auth:api')->prefix('dashboard')->group(function () {
     Route::apiResources([
         'admins' => AdminController::class,
         'users' => UserController::class,
